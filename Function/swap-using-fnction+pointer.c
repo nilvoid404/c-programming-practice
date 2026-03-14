093c36b5640be9884ea2d2a1398f27bc20c66907
Prince Karki <PrinceKarki121@gmail.com>
2026-03-14 18:01:31 +0545

    Swap numbers using function + Pointer

---
+++ Function/swap-using-fnction+pointer.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swap(int *a,int *b){
+    int temp;
+    
+    temp = *a;
+    *a = *b;
+    *b = temp;
+    
+}
+
+int main(){
+    
+    int x,y;
+    printf("Enter the value of x and y ");
+    scanf("%d%d",&x,&y);
+    printf("The value of x = %d and y = %d\n",x,y);
+    swap(&x,&y);
+    
+    printf("The value of x = %d and y = %d\n",x,y);
+    return 0;
+}
