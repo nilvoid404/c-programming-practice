6fa028c74bf175bb7e1d170ff345c7df7b70d3fa
Prince Karki <PrinceKarki121@gmail.com>
2026-03-22 08:30:45 +0545

    Addition of Simple Program

---
+++ Basic/addition-of-two-number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+ int a,b,sum;
+
+ printf("Enter value of a:\n");
+ scanf("%d",&a);
+
+ printf("Enter value of b:\n");
+ scanf("%d",&b);
+
+ sum = a+b;
+
+ printf("The sum of two numbers is %d\n",sum);
+ printf("Done by Prince Karki");
+    return 0;
+}
